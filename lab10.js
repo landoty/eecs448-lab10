fd4006811beb84f285a820401edfc5f2e69584aa
Landen Doty <landot2002@gmail.com>
2022-04-28 12:50:10 -0500

    added function to check post data client side

--- lab10.js
+++ lab10.js
@@ -6,3 +6,25 @@ function check_new_user() {
     }
     return true;
 }
+
+function check_new_post() {
+    new_post_form = document.getElementById("create_post");
+    username = new_post_form.children[1].value;
+    post_body = new_post_form.children[4].value;
+
+    if(!username) {
+        alert("Must enter a username!");
+        return false;
+    }
+    //Empty post body
+    else if(!post_body) {
+        alert("No empty posts!");
+        return false;
+    }
+    //No new post, just default text from create_post.html
+    else if(post_body == "Post text body...") {
+        alert("Please enter a new post!");
+        return false;
+    }
+    return true;
+}
