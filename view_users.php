1d15a97a8f05437ce519332e8ea9e2c7845e72a1
Landen Doty <landot2002@gmail.com>
2022-04-28 13:21:42 -0500

    start of php to show users

---
+++ view_users.php
@@ -0,0 +1,5 @@
+<?php 
+    //Create table and headers
+    echo "<table id=\"users_table\"><tr><td><strong>Username</strong></td><td><strong>ID</strong></td></tr>";
+    echo "<tr><td>Test User</td><td>1</td></table>";
+?>
