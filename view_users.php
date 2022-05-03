4ffa4c8d847d9db4da9a8bd770fa4851227140df
Landen Doty <landot2002@gmail.com>
2022-05-02 23:10:39 -0500

    implement backend for view users

--- view_users.php
+++ view_users.php
@@ -1,5 +1,23 @@
 <?php 
     //Create table and headers
-    echo "<table id=\"users_table\"><tr><td><strong>Username</strong></td><td><strong>ID</strong></td></tr>";
-    echo "<tr><td>Test User</td><td>1</td></table>";
+    echo "<table id=\"users_table\"><tr><td><strong>Username</strong></td></tr>";
+    //Create MySQL connection : url, username, password, db name
+    $mysqli = new mysqli("mysql.eecs.ku.edu", "l371d072", "boCh3quo", "l371d072");
+    //Check if an error occured when connecting
+    if($mysqli->connect_errno) {
+        $message = sprintf("Connect failed: %s\n", $mysqli->connect_error); 
+        $message = "<script> alert($message)</script>";
+        echo $message;
+    }
+    else {
+        $query = sprintf("select * from Users");
+        $result = $mysqli->query($query);
+        while($row = $result->fetch_assoc()) {
+            $user = $row["user_id"];
+            echo "<tr><td>$user</td></tr>";
+        }
+        echo "</table>";
+        $result->free();
+        $mysqli->close();
+    }
 ?>
