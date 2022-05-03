789c9f4801bca54beb9c43f7a075931d5b0d8c95
Landen Doty <landot2002@gmail.com>
2022-05-03 00:12:41 -0500

    implement backend for view user posts

--- view_user_posts.php
+++ view_user_posts.php
@@ -1,8 +1,28 @@
-<?php 
+<?php
     if(isset($_POST["user_dropdown"])) {
         $load_user = $_POST["user_dropdown"];
         unset($_POST["user_dropdown"]);
-        include "./view_user_posts.html"; 
-        echo "<div id=\"user-posts\"><p>$load_user</p></div>";
+        include "./view_user_posts.html";
+        //Create MySQL connection : url, username, password, db name
+        $mysqli = new mysqli("mysql.eecs.ku.edu", "l371d072", "boCh3quo", "l371d072");
+        if($mysqli->connect_errno) {
+            $message = sprintf("Connect failed: %s\n", $mysqli->connect_error); 
+            $message = "<script> alert($message)</script>";
+            echo $message;
+        }
+        else {
+            $query = sprintf("select post_id, content from Posts where author_id='%s'", $load_user);
+            $result = $mysqli->query($query);
+            echo "<h2>User: $load_user</h2>";
+            echo "<table id=\"user_posts\"><tr><th>Post ID</th><th>Content</th></tr>";
+            while($row = $result->fetch_assoc()) {
+                $post_id = $row["post_id"];
+                $content = $row["content"];
+                echo "<tr><td>$post_id</td><td>$content</td></tr>";
+            }
+            echo "</table>";
+            $result->free();
+            $mysqli->close();
+        }
     }
 ?>
